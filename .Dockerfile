1312607139ff99d446e9523bb210215c23b3f787
Ranjan Dailata <ranjancse@gmail.com>
2024-12-15 12:19:42 +0530

    Initial Commit

---
+++ .Dockerfile
@@ -0,0 +1,22 @@
+
+# Use the latest official Python runtime in slim variant
+FROM python:slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app
+COPY . .
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Run the application
+CMD ["python", "main.py"]
+
---
+++ restack_up.py
@@ -0,0 +1,76 @@
+import os
+from restack_sdk_cloud import RestackCloud
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    # Initialize the RestackCloud client with the SDK token from environment variables
+    restack_cloud_client = RestackCloud(os.getenv('RESTACK_CLOUD_TOKEN'))
+
+    engine = {
+        'name': 'restack_engine', # IMPORTANT: This must match the name of the engine in the Restack Cloud console.
+        'image': 'ghcr.io/restackio/restack:main',
+        'portMapping': [
+            {
+                'port': 5233,
+                'path': '/',
+                'name': 'engine-frontend',
+            },
+            {
+                'port': 6233,
+                'path': '/api',
+                'name': 'engine-api',
+            }
+        ],
+        'environmentVariables': [
+          {
+              'name': 'RESTACK_ENGINE_ID',
+              'value': os.getenv('RESTACK_ENGINE_ID'),
+          },
+          {
+              'name': 'RESTACK_ENGINE_ADDRESS',
+              'value': os.getenv('RESTACK_ENGINE_ADDRESS'),
+          },
+          {
+              'name': 'RESTACK_ENGINE_API_KEY',
+              'value': os.getenv('RESTACK_ENGINE_API_KEY'),
+          },
+        ],
+    }
+
+    # Define the application configuration
+    app = {
+        'name': 'flask_gemini',
+        'gitUrl': 'https://github.com/ranjancse26/restack.demo',
+        'dockerFilePath': 'https://github.com/ranjancse26/restack.demo/.Dockerfile',
+        'dockerBuildContext': 'https://github.com/ranjancse26/restack.demo',
+        'environmentVariables': [
+            {
+                'name': 'RESTACK_ENGINE_ID',
+                'value': os.getenv('RESTACK_ENGINE_ID'),
+            },
+            {
+                'name': 'RESTACK_ENGINE_ADDRESS',
+                'value': os.getenv('RESTACK_ENGINE_ADDRESS'),
+            },
+            {
+                'name': 'RESTACK_ENGINE_API_KEY',
+                'value': os.getenv('RESTACK_ENGINE_API_KEY'),
+            }
+        ],
+    }
+
+    # Configure the stack with the applications
+    await restack_cloud_client.stack({
+        'name': 'development environment python',
+        'previewEnabled': False,
+        'applications': [engine,app],
+    })
+
+    # Deploy the stack
+    await restack_cloud_client.up()
+
+# Run the main function
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
